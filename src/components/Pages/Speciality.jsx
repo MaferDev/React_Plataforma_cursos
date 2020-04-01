842d32a26bf9aa08341df76f96c9f43220f2b453
MaryFerH <fer.hs19@gmail.com>
2020-04-01 01:29:46 -0500

    Crea todos los componentes de las paginas privadas

---
+++ src/components/Pages/Speciality.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Speciality = () => {
+  return (
+    <div className="ed-grid">
+      <h1>Especialida</h1>
+    </div>
+  )
+}
+
+export default Speciality
---
+++ src/components/Pages/Teaches.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Teaches = () => {
+  return (
+    <div className="ed-grid">
+      <h1>Profesores</h1>
+    </div>
+  )
+}
+
+export default Teaches
