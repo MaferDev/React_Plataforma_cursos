e4513df0856c0466172682f0f67fff1db12c4a9c
MaryFerH <fer.hs19@gmail.com>
2020-04-01 01:26:51 -0500

    Configuración de redux y todas sus dependencias.

---
+++ src/redux/actionCreators.js
@@ -0,0 +1,78 @@
+import Axios from "axios"
+import { 
+  GET_ALL_POSTS, 
+  GET_ALL_SPECIALITIES, 
+  GET_ALL_COURSES,
+  GET_ALL_TEACHERS,
+  GET_POST,
+  GET_SPECIALITY,
+  GET_COURSE,
+  GET_FRAGMENT
+} from "./actions"
+
+const API_URL = process.env.REACT_APP_API_URL
+
+export const getAllPosts =()=>dispatch =>{
+  Axios.get(`${API_URL}/posts`)
+    .then(response=>dispatch({
+      type:GET_ALL_POSTS,
+      posts:response.data
+    }))
+}
+
+export const getAllSpecilities =()=>dispatch =>{
+
+  Axios.get(`${API_URL}/especialidades`)
+    .then(response=>dispatch({
+      type:GET_ALL_SPECIALITIES,
+      specialities:response.data
+    }))
+}
+
+export const getAllCourses =()=>dispatch =>{
+  Axios.get(`${API_URL}/cursos`)
+    .then(response=>dispatch({
+      type:GET_ALL_COURSES,
+      courses:response.data
+    }))
+}
+
+export const getAllTeachers =()=>dispatch =>{
+  Axios.get(`${API_URL}/profesores`)
+    .then(response=>dispatch({
+      type:GET_ALL_TEACHERS,
+      teachers:response.data
+    }))
+}
+
+export const getPost = id => dispatch =>{
+  Axios.get(`${API_URL}/posts/${id}`)
+    .then(response=>dispatch({
+      type:GET_POST,
+      post:response.data
+    }))
+}
+
+export const getSpeciality = id => dispatch =>{
+  Axios.get(`${API_URL}/especialidades/${id}`)
+    .then(response=>dispatch({
+      type:GET_SPECIALITY,
+      speciality:response.data
+    }))
+}
+
+export const getCourse = id => dispatch =>{
+  Axios.get(`${API_URL}/cursos/${id}`)
+    .then(response=>dispatch({
+      type:GET_COURSE,
+      course:response.data
+    }))
+}
+
+export const getFragment = id => dispatch =>{
+  Axios.get(`${API_URL}/clase/${id}`)
+    .then(response=>dispatch({
+      type:GET_FRAGMENT,
+      fragment:response.data
+    }))
+}
